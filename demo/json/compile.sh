97fc35d9746c584adf3195118197504f93ff71c1
ChijinZ <tlock.chijin@gmail.com>
2019-06-19 22:58:28 +0800

    init repository

---
+++ demo/json/compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+J8=-j8
+CLANGPP=clang++-7
+LLVMLINK=llvm-link-7
+OPT=opt-7
+REPO=https://github.com/nlohmann/json.git
+REVISION=b04543ecc58188a593f8729db38c2c87abd90dc3
+
+CXXFLAGS='-g -flto'
+
+[[ -d repo ]] || git clone "$REPO" repo
+mkdir -p repo/bitcode
+
+cd repo
+git checkout -f "$REVISION"
+
+cp test/src/fuzzer-parse_json.cpp ./target.cc
+"$CLANGPP" $CXXFLAGS target.cc  -I src -c -o bitcode/target.o
+
+cd bitcode
+"$LLVMLINK" *.o -o combined.bc
+"$OPT" -load=$VISFUZZ_BUILD/libVisFuzz.so -visfuzz -fvisfuzz-export=static.json \
+       combined.bc -o instru.bc
+"$CLANGPP" -g -fuse-ld=lld instru.bc $VISFUZZ_BUILD/libVisFuzzDriver.a -pthread -o app
+
+cp ./{app,static.json} ../
---
+++ demo/re2/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+J8=-j8
+CLANGPP=clang++-7
+LLVMLINK=llvm-link-7
+OPT=opt-7
+CXXFLAGS='-g -flto'
+
+[[ -d repo ]] || git clone https://github.com/google/re2.git repo
+mkdir -p repo/bitcode
+cd repo
+cp ../target.cc ./
+git checkout -f 499ef7eff7455ce9c9fae86111d4a77b6ac335de
+CXX="$CLANGPP" CXXFLAGS="$CXXFLAGS" make "$J8" obj/libre2.a
+"$CLANGPP" $CXXFLAGS ./target.cc -c -o bitcode/target.o -I .
+cd bitcode
+ar x ../obj/libre2.a
+"$LLVMLINK" *.o -o combined.bc
+"$OPT" -load=$VISFUZZ_BUILD/libVisFuzz.so -visfuzz -fvisfuzz-export=static.json \
+       combined.bc -o instru.bc
+"$CLANGPP" -g -fuse-ld=lld instru.bc $VISFUZZ_BUILD/libVisFuzzDriver.a -pthread -o app
+cp {app,static.json} ..
+
